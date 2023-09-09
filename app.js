7a4a6f6d636fd9a40408b54742c438666f6924af
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-09-09 12:39:17 +0100

    Updated the UTC time

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ console.log(currentDay)
 
 const currentUtcTime = new Date().toISOString();
 console.log(currentUtcTime);
+console.log(`2023-08-21T15:04:05Z`)
+
+const now = new Date();
+const year = now.getFullYear();
+const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+const day = String(now.getDate()).padStart(2, '0');
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+const seconds = String(now.getSeconds()).padStart(2, '0');
+
+const formattedTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+
+console.log(formattedTime);
 
 app.get("/api", (req, res) => {
     res.status(200).json({
         slack_name : req.query.slack_name,
         current_day : currentDay,
-        utc_time : currentUtcTime,
+        utc_time : formattedTime,
         track : req.query.track,
         github_file_url : user.github_file_url,
         github_repo_url : user.github_repo_url,
