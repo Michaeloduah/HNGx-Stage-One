d64936cc9fc879eb579b1251f24758286bcb6334
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-09-09 12:26:25 +0100

    Updated my return type

--- app.js
+++ app.js
@@ -17,21 +17,17 @@ console.log(currentDay)
 const currentUtcTime = new Date().toISOString();
 console.log(currentUtcTime);
 
-const getUser = (req, res) => {
+app.get("/api", (req, res) => {
     res.status(200).json({
-        data: {
-            slack_name : req.query.slack_name,
-            current_day : currentDay,
-            utc_time : currentUtcTime,
-            track : req.query.track,
-            github_file_url : user.github_file_url,
-            github_repo_url : user.github_repo_url,
-            status_code : user.status_code,
-        }
+        slack_name : req.query.slack_name,
+        current_day : currentDay,
+        utc_time : currentUtcTime,
+        track : req.query.track,
+        github_file_url : user.github_file_url,
+        github_repo_url : user.github_repo_url,
+        status_code : user.status_code,
     })
-}
-
-app.get('/api', getUser);
+})
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
